a1976481b700de305cba66071f4b0835c667a39a
Simply Done <muhammadmominov2004@gmail.com>
2024-10-10 23:49:08 +0500

    Postman

---
+++ Register_Postman/accounts/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email is not given.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError("Superuser must have is_staff = True")
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError("Superuser must have is_superuser = True")
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=128, null=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def has_perm(self, perm, obj=None):
+        return True
